d270b06ded81529627266c7336c9f08a785a6d31
SaloniSS <sxs170015@utdallas.edu>
2020-09-26 23:32:04 -0500

    Base Page for Home with lots of html

---
+++ src/pages/RecipesPage.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+function RecipesPage() {
+  return (
+    <div>
+      <h1>Recipe Title</h1>
+      <img
+        src="https://preppykitchen.com/wp-content/uploads/2019/06/Chocolate-cake-recipe-1200a.jpg"
+        alt="recipe"
+        width="150"
+        height="150"
+      />
+      <h3>Ingredients</h3>
+      <ul>
+        <li>1 ½ cups All purpose flour</li>
+        <li>1 cup Granulated Sugar</li>
+        <li>1/2 cup Cocoa powder</li>
+        <li>1 teaspoon Baking soda</li>
+        <li>1/2 teaspoon Salt</li>
+        <li>1 cup Milk</li>
+        <li>½ cup Oil</li>
+        <li>1 teaspoon Pure Vanilla Extract</li>
+        <li>2 teaspoon Vinegar</li>
+      </ul>
+      <h3>Recipe</h3>
+      <ol>
+        <li>Preheat oven to 350 degree F or 180 degree C.</li>
+        <li>
+          Line the cupcake tray with cupcake liners. If using cake pan , line it
+          with parchment circle.
+        </li>
+        <li>
+          Sieve and Mix all dry ingredients (All purpose flour, cocoa powder,
+          baking soda and salt) except sugar in a bowl. Always sieve from a
+          height for tender crumb outcome
+        </li>
+        <li>Add vinegar to milk and let it stand for a minute .</li>
+        <li>
+          Add granulated sugar to milk and vinegar mixture and whip for 1 minute
+          until the sugar is almost melted.{" "}
+        </li>
+        <li>
+          Now add in oil . Whip the liquid ingredients until oil is completely
+          blended well .
+        </li>
+        <li>
+          Mix liquid in the dry ingredients - whip with a electric whipper or by
+          a spatula or hand whip only till the batter is smooth and no lumps -
+          don't over beat - very important.
+        </li>
+        <li>
+          Bake in pre-heated oven for about 30-40 minutes or till toothpick
+          inserted in center of cupcake will come out with few dry crumbs .For
+          cake it's 30-40 minutes. Don't over bake.
+        </li>
+        <li>
+          Remove it from oven and after 5 minutes remove cake on wire rack and
+          let them cool completely before frosting.
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+export default RecipesPage;
