e33a8d1c689f598a33127ee647eceb0001c4dd87
SaloniSS <sxs170015@utdallas.edu>
2020-09-26 23:31:36 -0500

    Skeleton

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import RecipesPage from "./pages/RecipesPage";
+
+function App() {
+  return <RecipesPage />;
+}
+
+export default App;
