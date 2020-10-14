6497389090fbd02edbbf98b7e4bda41f254e78cc
SaloniSS <sxs170015@utdallas.edu>
2020-10-13 19:55:52 -0500

    set up firebase but I made some mistakes

--- src/components/db.js
+++ src/components/db.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
   databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
+  projectId: "acm-recipe",
   storageBucket: process.env.storageBucket,
   messagingSenderId: process.env.messagingSenderId,
   appId: process.env.appId,
